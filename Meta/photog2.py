d904a6c92c817da58aa2c7045740eeb8fa929b27
CybSec113 <71106462+CybSec113@users.noreply.github.com>
2024-03-25 17:23:03 -0400

    works for 28/39 test cases times out on 11 test cases
    proposal: modify sub-for-loop boundaries based on X and Y

--- Meta/photog2.py
+++ Meta/photog2.py
@@ -16,57 +16,55 @@ def getArtisticPhotographCount(N: int, C: str, X: int, Y: int) -> int:
       A.append(i)
     elif C[i] == 'B':
       B.append(i)
-  #print(P, A, B)
+  #print(P)
+  #print(A)
+  #print(B)
 
   P_len = len(P)
   A_len = len(A)
   B_len = len(B)
 
   #PAB
+  #print("PAB")
   for p in range(0, P_len):
-     for a in range(0, A_len):
-       if A[a] >= P[p]:
+    for a in range(0, A_len):
+      if (A[a] > P[p]):
+       if (abs(A[a]-P[p]) >= X) and (abs(A[a]-P[p]) <= Y):
         for b in range(0, B_len):
-          if B[b] >= A[a]:
-            #print("a", P[p], A[a], B[b])
-            front = abs(A[a]-P[p])
-            back = abs(B[b]-A[a])
-            if front >= X:
-              if front <= Y:
-                if back >= X:
-                  if back <= Y:
-                    result += 1
-                    #print("^")
+          if (B[b] > A[a]):
+            if (abs(B[b]-A[a]) >= X) and (abs(B[b]-A[a]) <= Y):
+              #print("photo", P[p], A[a], B[b])
+              result += 1
 
   #BAP
+  #print("BAP")
   for b in range(0, B_len):
     for a in range(0, A_len):
-      if A[a] >= B[b]:
-        for p in range(0, P_len):
-          if P[p] >= A[a]:
-            #print("b", B[b], A[a], P[p])
-            front = abs(A[a]-B[b])
-            back = abs(P[p]-A[a])
-            if front >= X:
-              if front <= Y:
-                if back >= X:
-                  if back <= Y:
-                    result += 1
-                    #print("^")
+      if A[a] > B[b]:
+        if (abs(A[a]-B[b]) >= X) and (abs(A[a]-B[b]) <= Y):
+          for p in range(0, P_len):
+            if P[p] > A[a]:
+              if (abs(P[p]-A[a]) >= X) and (abs(P[p]-A[a]) <= Y):
+                #print("photo", B[b], A[a], P[p])
+                result += 1
 
   return result
 
 if __name__ == '__main__':
   photo1='PAB.'
   C=''
-  N=2000      # max: 300,000
-  C_test = '.PBAAP.B'
-  N_test = 7
+  N=15000      # max: 300,000
+  C_test1 = 'APABA'
+  N_test1=5
+  C_test3 = '.PBAAP.B'
+  N_test3 = 8
 
-  for i in range(1, N):
+  for i in range(0, N):
     C += random.choice(photo1)
+  #print(C)
+  #print("len", len(C))
 
   start = timeit.default_timer()
   print("result", getArtisticPhotographCount(N, C, 1, 3))
   stop = timeit.default_timer()
-  print("time:", stop-start)
+  print("runtime (secs):", stop-start)
