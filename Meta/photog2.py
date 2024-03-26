f76980f38708ed6e0da40231d4009a591e047506
CybSec113 <71106462+CybSec113@users.noreply.github.com>
2024-03-26 13:32:19 -0400

    Works on 33/39 cases time exceeded on 6 cases

--- Meta/photog2.py
+++ Meta/photog2.py
@@ -5,66 +5,71 @@ import timeit
 def getArtisticPhotographCount(N: int, C: str, X: int, Y: int) -> int:
   # Write your code here
   result=0
-  P = [] 
-  A = [] 
-  B = [] 
 
-  for i in range(0, len(C)):
-    if C[i] == 'P':
-      P.append(i)
-    elif C[i] == 'A':
-      A.append(i)
-    elif C[i] == 'B':
-      B.append(i)
-  #print(P)
-  #print(A)
-  #print(B)
-
-  P_len = len(P)
-  A_len = len(A)
-  B_len = len(B)
-
-  #PAB
-  #print("PAB")
-  for p in range(0, P_len):
-    for a in range(0, A_len):
-      if (A[a] > P[p]):
-       if (abs(A[a]-P[p]) >= X) and (abs(A[a]-P[p]) <= Y):
-        for b in range(0, B_len):
-          if (B[b] > A[a]):
-            if (abs(B[b]-A[a]) >= X) and (abs(B[b]-A[a]) <= Y):
-              #print("photo", P[p], A[a], B[b])
-              result += 1
-
-  #BAP
-  #print("BAP")
-  for b in range(0, B_len):
-    for a in range(0, A_len):
-      if A[a] > B[b]:
-        if (abs(A[a]-B[b]) >= X) and (abs(A[a]-B[b]) <= Y):
-          for p in range(0, P_len):
-            if P[p] > A[a]:
-              if (abs(P[p]-A[a]) >= X) and (abs(P[p]-A[a]) <= Y):
-                #print("photo", B[b], A[a], P[p])
-                result += 1
+  for a in range(0, len(C)):
+    if C[a] == 'A':
+      p=max(0, a-1)
+      while p>=0:
+        if abs(a-p)>Y:
+          break
+        if abs(a-p)<X:
+          p-=1
+          continue
+        if C[p] == 'P':
+          b=min(a+1, len(C))
+          while b<len(C):
+            if abs(b-a)>Y:
+              break
+            if abs(b-a)<X:
+              b+=1
+              continue
+            if (C[b]=='B'):
+              print("photo:", p, a, b)
+              result+=1
+            b+=1
+        p-=1
+              
+      b=max(0, a-1)
+      while b>=0:
+        if abs(a-b)>Y:
+          break
+        if abs(a-b)<X:
+          b-=1
+          continue
+        if C[b] == 'B':
+          p=min(a+1, len(C))
+          while p<len(C):
+            if abs(p-a)>Y:
+              break
+            if abs(p-a)<X:
+              p+=1
+              continue
+            if (C[p]=='P'):
+              print("photo:", b, a, p)
+              result+=1
+            p+=1
+        b-=1
 
   return result
 
 if __name__ == '__main__':
   photo1='PAB.'
   C=''
-  N=15000      # max: 300,000
-  C_test1 = 'APABA'
-  N_test1=5
-  C_test3 = '.PBAAP.B'
-  N_test3 = 8
+  N=100      # max: 300,000
+  C_test1 = '.PBAAP.B'
+  N_test1 = 8
+  C_test2 = 'BBBPPP..A.PAB.PAAABBA.PAB..PPPPAPABBB.BPAAPP.PAAAAA..B.BAB.ABA..PPBBAB.P.P.PPB.PABPP..BAPP..B.BP.B..'
+  N_test2 = 100
+  #          01234567890123456789
+  C_test3 = '.B.BA..P..A..B......'
+  N_test3 = 20
 
-  for i in range(0, N):
-    C += random.choice(photo1)
+  #for i in range(0, N):
+  #  C += random.choice(photo1)
   #print(C)
   #print("len", len(C))
 
   start = timeit.default_timer()
-  print("result", getArtisticPhotographCount(N, C, 1, 3))
+  print("result", getArtisticPhotographCount(N_test3, C_test3, 2, 3))
   stop = timeit.default_timer()
   print("runtime (secs):", stop-start)
