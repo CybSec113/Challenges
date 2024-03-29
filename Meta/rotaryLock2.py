1f0c123ce682e8610c12a76dce63f8113cc0e8df
CybSec113 <71106462+CybSec113@users.noreply.github.com>
2024-03-29 13:00:15 -0400

    Not sure why this is failing 30/32 test cases.

--- Meta/rotaryLock2.py
+++ Meta/rotaryLock2.py
@@ -2,30 +2,33 @@ from typing import List
 # Write any import statements here
 import timeit
 import random
+import math
 
 def getMinCodeEntryTime(N: int, M: int, C: List[int]) -> int:
     # Write your code here
     def minMoveTime(source: int, target: int, N1: int) -> int:
         diff = abs(source-target)
-        if diff > int(N1 / 2):
+        if diff >= math.ceil(N1/2):
             return N1-diff
         else:
             return diff
 
-    result=minMoveTime(1,C[0],N)
-    lock1=C[0]
+    result=0
+    lock1=1
     lock2=1
-    for i in range(1, M):
-        print(lock1, lock2, result)
+    for i in range(0, M):
+        print("i ===> ", i, C[i])
         min1=minMoveTime(lock1,C[i],N)
         min2=minMoveTime(lock2,C[i],N)
+        #print("mins", min1, min2)
         if min1 <= min2:
             result+=min1
             lock1=C[i]
         else:
             result+=min2
             lock2=C[i]
-    print(lock1, lock2, result)
+        print("locks", lock1, lock2, result)
+        print()
 
     return result
 
@@ -39,17 +42,19 @@ if __name__ == '__main__':
     N2=10
     M2=4
     C2=[9,4,4,8]
-    #ans: 9
-    N3=10
-    M3=10
-    C3=[1,9,2,8,3,7,4,6,5,5]
+    #ans: 8
+    N3=97
+    M3=2
+    #C3=[90,30,32,25,2,4,19,86,1,26]
+    C3=[1,50]
     #ans: ??
-    N4=100
+    N4=99
     M4=10
     C4=[]
-    for i in range(0,N4):
+    for i in range(0,M4):
         C4.append(random.choice(range(1,N4+1)))
 
+    print(C3)
     start = timeit.default_timer()
     print("result:", getMinCodeEntryTime(N3,M3,C3))
     stop = timeit.default_timer()
