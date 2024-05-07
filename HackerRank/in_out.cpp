adb8426d2b2c3de6a537cff776d4ceac743577d1
CybSec113 <71106462+CybSec113@users.noreply.github.com>
2024-05-07 00:12:23 -0400

    Started adding some C++ code to work towards C++ badge added basic_data_types.cpp, in_out.cpp, inherited.cpp

---
+++ HackerRank/in_out.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int in1, in2, in3;
+    cin >> in1 >> in2 >> in3;
+    cout << in1+in2+in3 << endl;
+
+    return 0;
+}
