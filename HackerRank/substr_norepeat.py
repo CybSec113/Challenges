e6bfaaf5a29c59b40deb1080d8d16a0d6ffad758
CybSec113 <71106462+CybSec113@users.noreply.github.com>
2024-05-09 22:58:17 -0400

    Added substr_norepeat.py, an interview question.  Don't have instructions. Don't know if code is correct.

--- HackerRank/substr_norepeat.py
+++ HackerRank/substr_norepeat.py
@@ -2,20 +2,42 @@ import math
 import os
 import random
 import re
+import itertools
+
+# for a given string, find all substrings without repeating characters
+# for example, aba: a, b, a, ab, ba, aba (note: substring of length 1 is always unique)
+# PENDING CORRECT SOLUTION
 
 def findSubstrings(s):
     # Write your code here
-
     result=set()
-    for i in range(len(s)):
-        a=s[i]
-        for j in range(i+1, len(s)):
-            if s[j] not in a:
-                a += s[j]
-                result.add(a)
-            
-    return len(result) + len(s)
+    subsets=list()
 
+    for i in range(2, len(s)):
+        subsets += list(itertools.combinations(range(len(s)), i))
+
+    for i in subsets:
+        a=s[i[0]]
+        for j in i:
+            if s[j] not in a:
+                a+=s[j]
+                result.add(a)
+
+    print(sorted(result))
+    return len(result)
+
+# test cases
 if __name__ == '__main__':
+
+    # test 1
     s='bcada'
-    print("result (12):", findSubstrings(s))
+    print("Test 1 (12):", findSubstrings(s))
+
+    # test 2
+    s='aaaaa'
+    print("Test 2 (5):", findSubstrings(s))
+
+    # test 3
+    # a, b, c, d, e, ab, ac, ad, ae, bc, bd, be, cd, ce, de, abc, abd, abe, bcd, bce, cde, abcd, acde, bcde, abcde
+    s='abcde'
+    print("Test 3 (32):", findSubstrings(s))
